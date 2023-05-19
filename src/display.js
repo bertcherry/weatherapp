9f6ae8d1524d110cca74a2932337b4691a439d65
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-05-18 17:02:16 -0700

    style and add gif container

--- src/display.js
+++ src/display.js
@@ -3,13 +3,22 @@ import WindowImage from './window.png';
 function initialDisplay() {
     const contentDiv = document.getElementById('content');
     
+    const windowDiv = document.createElement('div');
+    windowDiv.classList.add('window-frame');
+    contentDiv.appendChild(windowDiv);
+
     const windowContainer = document.createElement('div');
     windowContainer.classList.add('img-container');
     const windowImage = document.createElement('img');
     windowImage.src = WindowImage;
     windowImage.alt = 'Transparent window frame to show outside weather';
     windowContainer.appendChild(windowImage);
-    contentDiv.appendChild(windowContainer);
+    windowDiv.appendChild(windowContainer);
+
+    const gifContainer = document.createElement('div');
+    gifContainer.classList.add('img-container');
+    gifContainer.id = 'gif-container';
+    windowDiv.appendChild(gifContainer);
 
     const thermometerDiv = document.createElement('div');
     thermometerDiv.id = 'thermometer';
