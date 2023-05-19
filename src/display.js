84b8ce9f9367598daca0fd188c86a9cdef1cc6bd
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-05-18 17:56:16 -0700

    display weather gif

--- src/display.js
+++ src/display.js
@@ -1,4 +1,5 @@
 import WindowImage from './window.png';
+import { getGif } from './getgif';
 
 function initialDisplay() {
     const contentDiv = document.getElementById('content');
@@ -9,6 +10,7 @@ function initialDisplay() {
 
     const windowContainer = document.createElement('div');
     windowContainer.classList.add('img-container');
+    windowContainer.id = 'window-container';
     const windowImage = document.createElement('img');
     windowImage.src = WindowImage;
     windowImage.alt = 'Transparent window frame to show outside weather';
@@ -18,7 +20,11 @@ function initialDisplay() {
     const gifContainer = document.createElement('div');
     gifContainer.classList.add('img-container');
     gifContainer.id = 'gif-container';
+    const gif = document.createElement('img');
+    gif.id = 'weather-gif';
+    gifContainer.appendChild(gif);
     windowDiv.appendChild(gifContainer);
+    getGif('static');
 
     const thermometerDiv = document.createElement('div');
     thermometerDiv.id = 'thermometer';
--- src/getweather.js
+++ src/getweather.js
@@ -1,13 +1,16 @@
+import { getGif } from "./getgif";
+
 async function getWeather(location) {
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=6b6b22d6fbcc46a9a70183239231705&q=${location}`, {mode: 'cors'});
     const weatherData = await response.json();
-    console.log(weatherData);
+    
     const currentWeather = {
         condition: weatherData.current.condition.text,
         temperatureC: weatherData.current.temp_c,
         temperatureF: weatherData.current.temp_f,
     }
-    console.log(currentWeather);
+    
+    getGif(currentWeather.condition);
     return currentWeather;
 }
 
